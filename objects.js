3843c5e1d272c3df647abccdaf0cc0eb7047d9c3
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-06-09 11:01:48 -0600

    Adding completed objects and self eval

--- objects.js
+++ objects.js
@@ -6,31 +6,33 @@
 // file in your Terminal.
 
 foods = {
-  apples: 23, 
-  grapes: 507, 
+  apples: 23,
+  grapes: 507,
   eggs: 48
 }
 
-// Write code that prints all of the 'keys' of the foods variable 
+// Write code that prints all of the 'keys' of the foods variable
 // you created above:
 // YOUR CODE HERE
-
-// Write code that prints all of the 'values' of the foods variable 
+console.log(Object.keys(foods));
+// Write code that prints all of the 'values' of the foods variable
 // you created above:
 // YOUR CODE HERE
-
-// Write code that prints the value of the second food of the foods variable 
+console.log(Object.values(foods));
+// Write code that prints the value of the second food of the foods variable
 // you created above:
 // YOUR CODE HERE
-
-// Write code that adds a food to the foods object. 
+console.log(Object.values(foods)[1]);
+// Write code that adds a food to the foods object.
 // Then, print the updated object:
 // YOUR CODE HERE
+foods.oranges = 102;
+console.log(Object.keys(foods));
 
 
-#-------------------
+//#-------------------
 // Part 2: Email
-#-------------------
+//#-------------------
 
 
 // Think about all the pieces of information associated with one single email in your inbox.
@@ -40,20 +42,28 @@ foods = {
 // value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
 // Write code that logs your email object to the terminal.
+var email = {
+  sender: "Piper Jarrett",
+  senderEmail: "piper.monet01@gmail.com",
+  reciever: "Bryce Jarrett",
+  recieverEmail: "brycej98@gmail.com",
+  subject: "Grocery list",
+  Message: "We need to go to the store. Here is the list; apples, oranges, milk, gold fish, bagels"
+}
 
-
-// Write code that logs all of the 'keys' of the email object 
+// Write code that logs all of the 'keys' of the email object
 // you created above:
 // YOUR CODE HERE
-
-// Write code that logs all of the 'values' of the email object 
+console.log(Object.keys(email));
+// Write code that logs all of the 'values' of the email object
 // you created above:
 // YOUR CODE HERE
+console.log(Object.values(email));
 
 
-#-------------------
+//#-------------------
 // Part 3: Many Emails - CHALLENGE!
-#-------------------
+//#-------------------
 
 // LONG EXAMPLE:
 // Now that we've learned about Objects AND Arrays, we can combine them.
@@ -69,33 +79,60 @@ posts = ["image at beach", "holiday party", "adorable puppy", "video of cute bab
 // that relate to each individual Instagram post:
 
 
-posts = [
-  {
-    imageSrc: "./images/beach.png",
-    caption: "At the beach with my besties",
-    timestamp: "4:37 PM August 13, 2019",
-    number_likes: 0,
-    comments: []
-  }, 
-  {
-    imageSrc: "./images/holiday-party.png",
-    caption: "What a great holiday party omg",
-    timestamp: "11:37 PM December 31, 2019",
-    number_likes: 13,
-    comments: []
-  }
-];
+//posts = [
+ //{
+// imageSrc: "./images/beach.png",
+ //caption: "At the beach with my besties",
+ //timestamp: "4:37 PM August 13, 2019",
+// number_likes: 0,
+ //comments: []
+//},
+//{
+//  imageSrc: "./images/holiday-party.png",
+//  caption: "What a great holiday party omg",
+//  timestamp: "11:37 PM December 31, 2019",
+//  number_likes: 13,
+//  comments: []
+//}
+//];
 
-console.log(posts);
-console.log(posts[0]);
+//console.log(posts);
+//console.log(posts[0]);
 
 
-// The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-// Objects. Each of those Objects has 4 key-value pairs. This may LOOK 
+// The code snippet above shows an Array with 2 elements. Each element in the Array is a
+// Objects. Each of those Objects has 4 key-value pairs. This may LOOK
 // a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 // good to have some exposure before going into Mod 1.
 
 
-// YOU DO: Create an array of at least 3 EMAIL Objects, using the same 
+// YOU DO: Create an array of at least 3 EMAIL Objects, using the same
 // key-value pairs you used in your email Object above.
 // Then, print the email Array to the Terminal.
+var emailArray = [
+  {
+  sender: "Piper Jarrett",
+  senderEmail: "piper.monet01@gmail.com",
+  reciever: "Bryce Jarrett",
+  recieverEmail: "brycej98@gmail.com",
+  subject: "Grocery list",
+  Message: "We need to go to the store today. Here is a list; apples, oranges, milk, gold fish, bagels"
+},
+{
+sender: "Piper Jarrett",
+senderEmail: "piper.monet01@gmail.com",
+reciever: "Bryce Jarrett",
+recieverEmail: "brycej98@gmail.com",
+subject: "Grocery list",
+Message: "We need to go to the store today. Here is a list; apples, oranges, milk, gold fish, bagels"
+},
+{
+  sender: "Piper Jarrett",
+  senderEmail: "piper.monet01@gmail.com",
+  reciever: "Bryce Jarrett",
+  recieverEmail: "brycej98@gmail.com",
+  subject: "Grocery list",
+  Message: "We need to go to the store today. Here is a list; apples, oranges, milk, gold fish, bagels"
+}
+];
+console.log(emailArray);
